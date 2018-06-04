90814c8df7d4de58f2810a2e3335b6a7535250db
Adrian <amchang87@gmail.com>
2018-06-03 18:41:10 -0700

    WORKING MOVIE 0.7-0.8 acc on train

--- movie/train.py
+++ movie/train.py
@@ -6,42 +6,37 @@ import torch.optim as optim
 from tqdm import tqdm
 from torch.utils.data.dataset import Dataset
 from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+import logging
+logging.basicConfig(level=logging.INFO,
+    format='%(asctime)s %(levelname)s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+logger = logging.getLogger(__name__)
 
 # force the seed to be the same
 np.random.seed(0)
 
 def train_test_datasets(train_size=0.7):
-    print('Loading classifications as review_classifications')
-    with open('../../text-classification-in-pytorch-using-lstm/tc-class.npz', 'rb') as file:
+    logger.info('Loading reviews')
+    with open('./binary-reviews.npz', 'rb') as file:
+        reviews = np.load(file)['arr_0']
+    logger.info('Loading classifications')
+    with open('./binary-labels.npz', 'rb') as file:
         review_classifications = np.load(file)['arr_0']
-    print('Loading reviews as reviews_as_indexes')
-    with open('../../text-classification-in-pytorch-using-lstm/tc-reviews.npz', 'rb') as file:
-        reviews_as_indexes = np.load(file)['arr_0']
     ### CRITICAL https://gist.githubusercontent.com/Tushar-N/dfca335e370a2bc3bc79876e6270099e/raw/940e8a4146f6424f842e25d034c2c1ac3440f84d/pad_packed_demo.py
     ### Must use padded sequence for variable sizes
-    '''
-    stored = []
-    for row in reviews_as_indexes:
-        local = np.array(row)
-        local = local + 1
-        local.resize(500)
-        stored.append(local)
-
-    stored = np.array(stored)
-    with open('./tc-reviews.npz', 'wb') as file:
-        np.savez(file, stored)
-    '''
-
-    return reviews_as_indexes, review_classifications, reviews_as_indexes, review_classifications
-    #split = np.random.permutation(review_classifications.shape[0])
-    #amount = review_classifications.shape[0]
-    #test_id = split[int(amount * train_size):]
-    #train_id = split[:int(amount * train_size)]
-    #train = reviews_as_indexes[train_id, :]
-    #train_classify = review_classifications[train_id]
-    #test = reviews_as_indexes[test_id, :]
-    #test_classify = review_classifications[test_id]
-    #return train, train_classify, test, test_classify
+    record_count, record_features = reviews.shape[0], reviews.shape[1]
+    logger.info(f'Using {record_count} reviews of {record_features} words')
+    split = np.random.permutation(record_count)
+    amount = review_classifications.shape[0]
+    test_id = split[int(amount * train_size):]
+    train_id = split[:int(amount * train_size)]
+    train = reviews[train_id, :]
+    train_classify = review_classifications[train_id]
+    test = reviews[test_id, :]
+    test_classify = review_classifications[test_id]
+    return train, train_classify, test, test_classify
 
 class MovieDataset(Dataset):
 
@@ -51,7 +46,6 @@ class MovieDataset(Dataset):
         self._label = label
 
     def __getitem__(self, index):
-        print(self._data[index])
         return self._data[index], self._label[index]
 
     def __len__(self):
@@ -75,55 +69,67 @@ class Sentiment(nn.Module):
         self._hidden = (t.zeros(1, self._batch_size, self._hidden_dim).cuda(), t.zeros(1, self._batch_size, self._hidden_dim).cuda())
 
     def forward(self, reviews):
-        embedded_reviews = self._embedding(reviews)
+        # careful of leak index
+        lengths = []
+        for i, review in enumerate(reviews):
+            lengths.append((i, (review == 0).nonzero().data[0]))
+        #lengths = t.LongTensor(lengths).cuda()
+        reordered_reviews = t.zeros(reviews.shape).long().cuda()
+        lengths = sorted(lengths, key=lambda item: item[1], reverse=True)
+        non_zero_lengths = []
+        for i, (org_index, length) in enumerate(lengths):
+            reordered_reviews[i] = reviews[org_index]
+            non_zero_lengths.append(length)
+        non_zero_lengths = t.LongTensor(non_zero_lengths).cuda()
+        embedded_reviews = self._embedding(reordered_reviews).transpose(0, 1) # watch out here, it's b, s, l, needs to be s, b, l
+        embedded_reviews = pack_padded_sequence(embedded_reviews, non_zero_lengths)
         # output [-1] === hidden last
-        output, hidden = self._lstm(embedded_reviews.t(), self._hidden)
+        output, (ht, ct) = self._lstm(embedded_reviews, self._hidden) # use output, needs to pad_pack
         #self._hidden = (hidden[0].data.clone(), hidden[1].data.clone())
-        predictions = self._linear(output[-1])
+        predictions = self._linear(ht[-1])
         return predictions
 
-def train_model(model, dataloader, epochs = 1):
-    print('Training model')
+def train_model(model, dataloader, epochs = 4):
+    logger.info('Training model')
     model.train()
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr = 0.001)
     for epoch in tqdm(range(epochs)):
         for i, (movie_reviews, movie_labels) in enumerate(dataloader):
-            #print(movie_reviews, movie_labels)
-            predictions = model(t.Tensor([movie_reviews]).long().cuda())
             model.zero_grad() # DO NOT DO OPT ZERO_GRAD
-            loss = criterion(predictions, t.Tensor([movie_labels]).long().cuda())
+            predictions = model(movie_reviews)
+            loss = criterion(predictions, movie_labels.long().cuda())
             #print(predictions, movie_labels, loss)
             loss.backward()
             optimizer.step()
             if i % 100 == 0:
-                print(f'{i} {loss.item()}')n
+                logger.info(f'{i} {loss.item()}')
 
 def test_model(model, dataloader):
-    print('Testing Model')
+    logger.info('Testing Model')
     model.eval()
     correct = 0
     total = 0
-    for movie_review, movie_label in tqdm(dataloader):
-        movie_review = t.Tensor([movie_review]).long().cuda()
-        movie_label = t.Tensor([movie_label]).long().cuda()
-        preds = model.forward(movie_review)
+    for movie_reviews, movie_labels in tqdm(dataloader):
+        movie_reviews = movie_reviews.long().cuda()
+        movie_labels = movie_labels.long().cuda()
+        preds = model.forward(movie_reviews)
         val, idx = preds.max(1)
         total += idx.size()[0]
-        correct += idx.eq(movie_label.view_as(idx)).cpu().sum()
-    print(f'correct {correct.cpu().numpy() / total}')
+        correct += idx.eq(movie_labels.view_as(idx)).cpu().sum()
+    logger.info(f'correct {correct.cpu().numpy() / total}')
 
 if __name__ == '__main__':
-    print('Running program')
+    logger.info('Running program')
     train_x, train_y, test_x, test_y = train_test_datasets()
-    #WORD_TOTAL = 45636 + 1
-    WORD_TOTAL = 50000
-    REVIEW_LENGTH = 5000
-    BATCH_SIZE = 1 ## See above note for critical for variable sizes
-    movie_dataset = MovieDataset(train_x[:10], train_y[:10])
+    WORD_TOTAL = 45636 + 1
+    #WORD_TOTAL = 50000
+    REVIEW_LENGTH = 500
+    BATCH_SIZE = 500 ## See above note for critical for variable sizes
+    movie_dataset = MovieDataset(train_x[:1000], train_y[:1000])
     movie_dataloader = DataLoader(movie_dataset, batch_size=BATCH_SIZE)
     model = Sentiment(REVIEW_LENGTH, WORD_TOTAL, BATCH_SIZE).cuda()
     train_model(model, movie_dataloader)
-    t.save(model.state_dict(), './model-3.pt')
-    #model.load_state_dict(t.load('./model-2.pt'))
+    t.save(model.state_dict(), './model-final.pt')
+    model.load_state_dict(t.load('./model-final.pt'))
     test_model(model, movie_dataloader)
